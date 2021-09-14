d1cb98d14ecfb2b957f6ce6a2041b574df5ad7f9
Matt Lewis <matt@mplewis.com>
2021-09-13 21:22:06 -0600

    refactor: uninstall pagination

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from "vue";
-import Pagination from "v-pagination-3";
 
 import { store, key } from "./store";
 import App from "./App.vue";
 import TorrentView from "./components/TorrentView.vue";
 
 const app = createApp(App);
-app.component("pagination", Pagination);
 app.component("TorrentView", TorrentView);
 app.use(store, key);
 app.mount("#app");
